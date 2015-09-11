32b1526808c51f1ad0bfc541639b60a7aac13a26
ShevchenkoV <elanceShevchenko@gmail.com>
2015-09-11 04:48:54 +0300

    added grunt, tasks for uglify and minify, changed directories

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    uglify: {
+      options: {
+        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+      },
+      build: {
+        files: {
+          'dist/bdrp-sliced.min.js': 'src/daterangepicker.js'
+        }
+      }
+    },
+    cssmin: {
+      options: {
+        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+      },
+      build: {
+        files: {
+          'dist/bdrp-sliced.min.css': 'src/daterangepicker.css'
+        }
+      }
+    }
+
+  });
+  grunt.registerTask('default', ['uglify', 'cssmin']);
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+};
